1f33842501beaa17038ea4f0f2f103a5291a882d
jarekratajski <jratajski@gmail.com>
2017-08-03 20:24:42 +0200

    added ktor repositories

---
+++ src/main/kotlin/ch/komunumo/server/Main.kt
@@ -0,0 +1,19 @@
+package ch.komunumo.server
+
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.response.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
+
